7fc64f751df0f2414312311042d7259a735b8524
riqup <rikup83@hotmail.com>
2018-10-03 15:47:08 +0300

    Eka commit!

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-export class Home extends Component {
+export class Home extends Component {               // ETUSIVU
   displayName = Home.name
 
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
+        <h1>Hello, world! This is my first Demo.</h1>
         <p>Welcome to your new single-page application, built with:</p>
         <ul>
           <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
